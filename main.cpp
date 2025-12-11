c7cdba247954b03fa815bc03a3fe2724bd552c1c
darshdesai00 <darshdesai50@icloud.com>
2025-12-10 18:19:39 -0800

    Working on Milestone 1

--- main.cpp
+++ main.cpp
@@ -4,8 +4,29 @@
 #include <map>
 using namespace std;
 
+// Milestone 1
 int main() {
 
+    map<string , int> airports;
+
+    ifstream file("210-final-1-FA25.txt");
+    string origin, destination;
+
+    while (file >> origin >> destination) {
+
+        if(airports.count(origin) == 0) { 
+            ariports(origin) = 0
+        }
+
+        if(airports.count(destination) == 0) { 
+            airports[destination] = 0        
+        }
+
+        airports(origin)
+        airports(destination) = 0
+    
+    
+    }
     // Initial commit: 
     cout << "Airport traffic.\n";
 
